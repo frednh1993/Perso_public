c69db4c3f57a0bc6cc6909b8af47a0d759104d12
frednh1993 <Frederik.Boutin@uqar.ca>
2023-04-15 16:07:33 -0400

    Authentification fonctionne

--- Technologies_du_commerce_électronique/9. Authentification/Jwt2/Jwt2/Controllers/AuthController.cs
+++ Technologies_du_commerce_électronique/9. Authentification/Jwt2/Jwt2/Controllers/AuthController.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
+﻿//using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
@@ -14,11 +14,14 @@ namespace Jwt2.Controllers
     {
         public static User user = new User();
         private readonly IConfiguration _configuration;
+        //private readonly IUserService _userService;
+
 
         public AuthController(IConfiguration configuration)
         {
             _configuration = configuration;
         }
+
 
         [HttpPost("register")]
         public async Task<ActionResult<User>> Register(UserDto request)
@@ -31,6 +34,7 @@ namespace Jwt2.Controllers
 
             return Ok(user);
         }
+
 
         [HttpPost("login")]
         public async Task<ActionResult<string>> Login(UserDto request)
@@ -45,14 +49,70 @@ namespace Jwt2.Controllers
                 return BadRequest("Wrong password.");
             }
             string token = CreateToken(user);
+
+            //var refreshToken = GenerateRefreshToken();
+            //SetRefreshToken(refreshToken);
+
             return Ok(token);
         }
+
+
+        //[HttpPost("refresh-token")]
+        //public async Task<ActionResult<string>> RefreshToken()
+        //{
+        //    var refreshToken = Request.Cookies["refreshToken"];
+
+        //    if (!user.RefreshToken.Equals(refreshToken))
+        //    {
+        //        return Unauthorized("Invalid Refresh Token.");
+        //    }
+        //    else if (user.TokenExpires < DateTime.Now)
+        //    {
+        //        return Unauthorized("Token expired.");
+        //    }
+
+        //    string token = CreateToken(user);
+        //    var newRefreshToken = GenerateRefreshToken();
+        //    SetRefreshToken(newRefreshToken);
+
+        //    return Ok(token);
+        //}
+
+
+        //private RefreshToken GenerateRefreshToken()
+        //{
+        //    var refreshToken = new RefreshToken
+        //    {
+        //        Token = Convert.ToBase64String(RandomNumberGenerator.GetBytes(64)),
+        //        Expires = DateTime.Now.AddDays(7),
+        //        Created = DateTime.Now
+        //    };
+
+        //    return refreshToken;
+        //}
+
+
+        //private void SetRefreshToken(RefreshToken newRefreshToken)
+        //{
+        //    var cookieOptions = new CookieOptions
+        //    {
+        //        HttpOnly = true,
+        //        Expires = newRefreshToken.Expires
+        //    };
+        //    Response.Cookies.Append("refreshToken", newRefreshToken.Token, cookieOptions);
+
+        //    user.RefreshToken = newRefreshToken.Token;
+        //    user.TokenCreated = newRefreshToken.Created;
+        //    user.TokenExpires = newRefreshToken.Expires;
+        //}
+
 
         private string CreateToken(User user)
         {
             List<Claim> claims = new List<Claim>
             {
-                new Claim(ClaimTypes.Name, user.Username)
+                new Claim(ClaimTypes.Name, user.Username),
+                new Claim(ClaimTypes.Role, "Admin")
             };
 
             var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value));  
@@ -69,6 +129,7 @@ namespace Jwt2.Controllers
             return jwt;
         }
 
+
         private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt) 
         { 
             using(var hmac = new HMACSHA512())
@@ -79,6 +140,7 @@ namespace Jwt2.Controllers
             }
         }
 
+
         private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
         {
             using (var hmac = new HMACSHA512(passwordSalt))
@@ -86,8 +148,6 @@ namespace Jwt2.Controllers
                 var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
                 return computedHash.SequenceEqual(passwordHash);
             }
-
-
         }
     }
 }
